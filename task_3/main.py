2034ada6b3a545778100ae1b53f952c6b7702429
TSCRM\m.hryhoriev <m.hryhoriev@creatio.com>
2024-10-05 17:05:21 +0300

    Added information into code

--- task_3/main.py
+++ task_3/main.py
@@ -16,6 +16,7 @@ def hanoi_recursive(n, source_rod, destination_rod, auxiliary_rod, rods_state):
         print(f"Move the disk from {source_rod} to {destination_rod}: {disk}")
         return
 
+    # Move n-1 disks from source to auxiliary
     hanoi_recursive(n - 1, source_rod, auxiliary_rod, destination_rod, rods_state)
 
     disk = rods_state[source_rod].pop()
@@ -24,6 +25,7 @@ def hanoi_recursive(n, source_rod, destination_rod, auxiliary_rod, rods_state):
     print(f"Move the disk from {source_rod} to {destination_rod}: {disk}")
     print(f"Intermediate state: {rods_state}")
 
+    # Move the n-1 disks from auxiliary to destination
     hanoi_recursive(n - 1, auxiliary_rod, destination_rod, source_rod, rods_state)
 
 
